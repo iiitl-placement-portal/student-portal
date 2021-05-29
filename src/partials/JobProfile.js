8f0b1d9f69f8eb4f96f24c352e94c03c667a04b7
blank-27 <lcs2019045@iiitl.ac.in>
2021-05-30 00:34:30 +0530

    job profile connected with api

--- src/partials/JobProfile.js
+++ src/partials/JobProfile.js
@@ -6,9 +6,10 @@ import { Switch, Route, useLocation, Redirect,Link } from "react-router-dom";
 
 import "./css/styles.css";
 
-const applyForJob = async (id) => {
+const applyForJob = async (ret) => {
+    console.log("applying",ret);
     try {
-        const data = await fetch(`http://localhost:5000/jobs/${id}/apply`, {
+        const data = await fetch(`http://localhost:5000/jobs/${ret}/apply`, {
             method: "post",
             headers: {
                 Authorization:
@@ -22,78 +23,106 @@ const applyForJob = async (id) => {
     }
 }
 
-const JobProfile =  () =>{
-    return(
-        <div className="flex flex-col min-h-screen overflow-hidden">
-            {/*  Site header */}
-            <Header type="dashboard" />
+class JobProfile extends Component{
 
-            {/*  Page content */}
-            <div className="jobsProfile__section">
-                <main className="jobsProfile__container flex-grow">
-                    <div className="jobsProfile__header">
-                        <div className="jobsProfile__header-img">
-                            <SvgIcon src="banda.jpg" />
-                        </div>
-                        <h3>Positin name</h3>
-                        <p>Company name . complete location</p>
-                        <div className="jobsProfile__header-details">
-                            <div>
-                                <h6>Mini Heading</h6>
-                                <p>Lorem berste</p>
+    state = {
+        id:"abc"
+    }
+
+    // constructor(props)
+    // {
+    //     super(props);
+    //     this.state = { id : "" };
+    // }
+
+    componentDidMount(){
+        var pathy = window.location.pathname;
+        var ret = pathy.replace('/jobs/','');
+        // this.setState({id:ret});
+        this.setState({
+            id: ret
+        },() => {
+            console.log("insstate",this.state.id)
+        });
+    }
+
+    render(){
+        // console.log(this.state.id,"inrenrder")
+        var idi = this.state.id;
+        return(
+            <div className="flex flex-col min-h-screen overflow-hidden">
+                {/*  Site header */}
+                <Header type="dashboard" />
+    
+                {/*  Page content */}
+                <div className="jobsProfile__section">
+                    <main className="jobsProfile__container flex-grow">
+                        <div className="jobsProfile__header">
+                            <div className="jobsProfile__header-img">
+                                <SvgIcon src="banda.jpg" />
                             </div>
-                            <div>
-                                <h6>Mini Heading</h6>
-                                <p>Lorem berste</p>
+                            <h3>Positin name</h3>
+                            <p>Company name . complete location</p>
+                            <div className="jobsProfile__header-details">
+                                <div>
+                                    <h6>Mini Heading</h6>
+                                    <p>Lorem berste</p>
+                                </div>
+                                <div>
+                                    <h6>Mini Heading</h6>
+                                    <p>Lorem berste</p>
+                                </div>
+                                <div>
+                                    <h6>Mini Heading</h6>
+                                    <p>Lorem berste</p>
+                                </div>
                             </div>
-                            <div>
-                                <h6>Mini Heading</h6>
-                                <p>Lorem berste</p>
+                            <div className="jobsProfile__header-button">
+                                <ul className="flex flex-grow justify-end flex-wrap items-center">
+                                    <li>
+                                    <Link to="/home" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800">
+                                        <span onClick={() => applyForJob(idi)}>Apply Now</span>
+                                    </Link>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
-                        <div className="jobsProfile__header-button">
-                            <ul className="flex flex-grow justify-end flex-wrap items-center">
-                                <li>
-                                    <span><button onClick={applyForJob()}>Apply Now</button></span>
-                                </li>
-                            </ul>
+                        <div className="jobsProfile__main">  
+                            <div className="jobsProfile__main-heading">
+                                <h3>ABout the job</h3>
+                            </div>  
+                            <div className="jobsProfile__main-para">
+                                <h5>Description</h5>
+                                <p>
+                                    berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
+                                </p>
+                            </div>
+                            <div className="jobsProfile__main-para">
+                                <h5>Role</h5>
+                                <p>
+                                    berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
+                                </p>
+                            </div>
+                            <div className="jobsProfile__main-para">
+                                <h5>Qualification</h5>
+                                <p>
+                                    berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="jobsProfile__main">  
-                        <div className="jobsProfile__main-heading">
-                            <h3>ABout the job</h3>
-                        </div>  
-                        <div className="jobsProfile__main-para">
-                            <h5>Description</h5>
-                            <p>
-                                berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
-                            </p>
+                        
+                        <div className="jobsProfile__footer">
+                            <h3 className="jobsProfile__footer-header">Pay Range</h3>
+                            <p>Slaary info</p>
                         </div>
-                        <div className="jobsProfile__main-para">
-                            <h5>Role</h5>
-                            <p>
-                                berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
-                            </p>
-                        </div>
-                        <div className="jobsProfile__main-para">
-                            <h5>Qualification</h5>
-                            <p>
-                                berste Ziel unseres Unternehmens ist die Zufriedenheit unserer Kunden. Vom Moment der Online-Bestellung bis zur reibungslosen Koordination dieser Bestellung hinter den Kulissen wollen wir stets flexibel, agil und zielgerichtet auftreten. Daher lautet eines unserer zentralen Führungsprinzipien "Im Zweifel: Handeln"! Wir möchten, dass unsere Teams zusammenarbeiten, die 
-                            </p>
-                        </div>
-                    </div>
-                    
-                    <div className="jobsProfile__footer">
-                        <h3 className="jobsProfile__footer-header">Pay Range</h3>
-                        <p>Slaary info</p>
-                    </div>
-                </main>
+                    </main>
+                </div>
+                {/*  Site footer */}
+                <Footer />
+    
             </div>
-            {/*  Site footer */}
-            <Footer />
-
-        </div>
-    );
+        );
+    }
 }
 
 export default JobProfile;
