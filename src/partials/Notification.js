5733c458151febfa322fe3646307268c0297e9a7
the-rdt <rdtiwari.official@gmail.com>
2021-05-30 02:03:35 +0530

    notification section added

--- src/partials/Notification.js
+++ src/partials/Notification.js
@@ -1,22 +1,45 @@
-import React ,{Component, Profiler} from 'react';
+import React, { Component, Profiler } from "react";
 import "./css/styles.css";
-import Footer from "./../partials/Footer";
 
-const Notification = () => {
+const getNotification = async () => {
+  const data = await fetch("http://localhost:5000/profile", {
+    headers: {
+      Authorization:
+        "Bearer " + JSON.parse(localStorage.getItem("token")).token,
+    },
+  }).then((val) => val.json());
+  const notification = data.notification;
+//   console.log(notification);
+  return notification;
+};
 
-    return(
-        <div className="notification__section">
-            <h3 className="notification__heading">Notifications</h3>
-            <div className="notification__container">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna</p>
-            </div>
-        </div>
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notification: [],
+    };
+  }
+  componentDidMount() {
+    getNotification()
+      .then((val) => {
+        this.setState({ notification: val });
+      })
+      .catch((err) => {
+        console.error("Error in notification", err);
+      });
+  }
+  render() {
+    const allNotifications = this.state.notification.map((val) => {
+      return <p>{val.message}</p>;
+    });
+    return (
+      <div className="notification__section">
+        <h3 className="notification__heading">Notifications</h3>
+        <div className="notification__container">{allNotifications}</div>
+      </div>
     );
-} 
+  }
+}
 
 export default Notification;
