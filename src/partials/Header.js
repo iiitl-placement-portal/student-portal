a3a4d45c46089631b4013fa188236354ae263561
blank-27 <lcs2019045@iiitl.ac.in>
2021-06-15 23:36:07 +0530

    added tpo dashboard and all company page

--- src/partials/Header.js
+++ src/partials/Header.js
@@ -6,6 +6,8 @@ import SvgIcon from "./SvgIcon";
 function Header(props) {
   const [top, setTop] = useState(true);
 
+  // console.log(props.nav);
+
   // detect whether user has scrolled the page down by 10px
   useEffect(() => {
     const scrollHandler = () => {
@@ -124,7 +126,67 @@ function Header(props) {
         </div>
       </header>
     );
+  }else if(props.type ==="dashboardTpo"){
+    const Links = (props.nav).map(el => {
+      // console.log(el.name);
+      return(
+        <li key={el.name}>
+          <Link
+            to={"/"+ el.nav}
+            className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+          > 
+            {el.name}
+          </Link>
+        </li>
+      );
+    });
+    return(<header
+      className={`fixed w-full z-30 md:bg-opacity-90 transition duration-300 ease-in-out ${
+        !top && "bg-white blur shadow-lg"
+      }`}
+    >
+      <div className="max-w-6xl mx-auto px-5 sm:px-6">
+        <div className="flex items-center justify-between h-16 md:h-20">
+          {/* Site branding */}
+          <div className="flex-shrink-0 mr-4">
+            {/* Logo */}
+            <Link to="/" className="block" aria-label="Cruip">
+              <div className="header-logo">
+                <SvgIcon src="logo.png" />
+              </div>
+            </Link>
+          </div>
+
+          {/* Site navigation */}
+          <nav className="flex flex-grow">
+            <ul className="flex flex-grow justify-end flex-wrap items-center">
+              {Links}
+              <li>
+                <Link
+                  to="/logout"
+                  className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3"
+                >
+                  <span>Log Out</span>
+                  <svg
+                    className="w-3 h-3 fill-current text-gray-400 flex-shrink-0 ml-2 -mr-1"
+                    viewBox="0 0 12 12"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                      fillRule="nonzero"
+                    />
+                  </svg>
+                </Link>
+                {/* </li> */}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>);
   }
 }
+
 
 export default Header;
