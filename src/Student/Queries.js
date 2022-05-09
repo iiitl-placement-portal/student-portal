82a84fc8160a0ba4c908d7f11408824ba14c2f73
therdt <rdtiwari.official@gmail.com>
2022-05-09 06:23:31 +0530

    fix queries page

--- src/Student/Queries.js
+++ src/Student/Queries.js
@@ -1,77 +1,149 @@
-import React , {useState} from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState, useEffect, Component } from "react";
+import { useForm } from "react-hook-form";
+import { BASE_URL } from "../CONSTANTS";
 
-const Queries = ({title}) => {
-  const [formData, setFormData] = useState("")
+async function askQuery(question) {
+  try {
+    console.log(question);
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => {
-    console.log(data);
-    alert("query Submitted");
-    setFormData("");
+    const data = await fetch(`${BASE_URL}/students/ask-query`, {
+      method: "post",
+      headers: {
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("token")).token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question }),
+    });
+
+    if (data.status === 200) {
+      alert("Query submitted Successfully");
+    } else {
+      alert("Query could not be submitted");
+    }
+    // console.log(data);
+  } catch (err) {
+    console.error("Error in updating Linkedln URL", err);
+    return "error, please check console for details";
   }
-  console.log(errors);
+}
 
+class Queries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      queries: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(`${BASE_URL}/all-query`, {
+      method: "get",
+      headers: {
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("token")).token,
+      },
+    })
+      .then(val => val.json())
+      .then(data => this.setState({ queries: data }))
+      .then(() => {
+        console.log("all queries", this.state.queries);
+      });
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    askQuery(this.state.value);
+  }
+
+  // console.log(errors);
+
+  render() {
     return (
       <div className="flex flex-col overflow-hidden">
         {/*  Page content */}
         <div className="stuquery__section">
-          <h3 className="stuquery__section-heading">{title}</h3>
+          <h3 className="stuquery__section-heading">Queries</h3>
           <main className="stuquery__container flex-grow">
-            <form className='stuquery__form' onSubmit={handleSubmit(onSubmit)}>
-              <textarea {...register("query", {required: true})} />
+            <form className="stuquery__form" onSubmit={this.handleSubmit}>
+              <textarea value={this.state.value} onChange={this.handleChange} />
 
               <input type="submit" value={"Submit"} />
             </form>
 
             <div> </div>
-
           </main>
         </div>
+
         <div className="stuquery__section">
           <h3 className="stuquery__section-heading">Old Queries</h3>
+
           <main className="stuquery__container flex-grow">
-            
             <div className="query__section">
               <main className="query__container ">
-                
-                <div className="query__card"> 
-                  <form onSubmit={handleSubmit(onSubmit)}>
+                {this.state.queries?.map((query, ind) => {
+                  return (
+                    <div className="query__card" key={ind}>
+                      <form>
+                        <h6>{query.question}</h6>
+                        <div className="query__card-details">
+                          <textarea
+                            placeholder="has not been answered yet by TPO"
+                            value={query.answer}
+                            readOnly
+                            style={{ resize: false, overflowY: "scroll" }}
+                          />
+                        </div>
+                      </form>
+                    </div>
+                  );
+                })}
+
+                {/* <div className="query__card">
+                  <form onSubmit={handleSubmit(askQuery)}>
                     <h6>Ques. Are placements Online or offline ?</h6>
                     <div className="query__card-details">
-                      <textarea placeholder="type your answer...." value={"dasms smsdsa "} />
+                      <textarea
+                        placeholder="type your answer...."
+                        value={"dasms smsdsa dasd"}
+                      />
                     </div>
-                  </form> 
-                </div>
-                
-                <div className="query__card"> 
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <h6>Ques. Are placements Online or offline ?</h6>
-                    <div className="query__card-details">
-                      <textarea placeholder="type your answer...." value={"dasms smsdsa dasd"} />
-                    </div>
-                  </form> 
+                  </form>
                 </div>
 
-                <div className="query__card"> 
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                    <h6>Ques.  Are placements Online or offlineAre placements Online or offlineAre placements Online or offline Are placements Online or offline ?</h6>
+                <div className="query__card">
+                  <form onSubmit={handleSubmit(askQuery)}>
+                    <h6>
+                      Ques. Are placements Online or offlineAre placements
+                      Online or offlineAre placements Online or offline Are
+                      placements Online or offline ?
+                    </h6>
                     <div className="query__card-details">
-                      <textarea placeholder="type your answer...." value={"dasdasdas das dasd asd asd hume kya pta"}  />
+                      <textarea
+                        placeholder="type your answer...."
+                        value={"dasdasdas das dasd asd asd hume kya pta"}
+                      />
                     </div>
-                  </form> 
-                </div>
+                  </form>
+                </div> */}
               </main>
             </div>
-
           </main>
         </div>
       </div>
     );
+  }
 }
 
 export default Queries;
-{/* <div className="query__card"> 
+{
+  /* <div className="query__card"> 
   <form onSubmit={handleSubmit(onSubmit)}>
     <h6>Ques.  Are placements Online or offlineAre placements Online or offlineAre placements Online or offline Are placements Online or offline ?</h6>
     <h5>Asked by : EDT</h5>
@@ -81,4 +153,5 @@ export default Queries;
       <input className="query_submit" type="submit"  value={"Reply"} />
     </div>
   </form> 
-</div> */}
+</div> */
+}
